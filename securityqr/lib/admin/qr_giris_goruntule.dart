88212ad709d4aa5dbc35d50e54d06818f98a3ea3
samikzlbga005 <82725201+samikzlbga005@users.noreply.github.com>
2022-05-01 19:47:16 +0300

    qr ile kullanıcı giriş çıkışı gösterme

--- securityqr/lib/admin/qr_giris_goruntule.dart
+++ securityqr/lib/admin/qr_giris_goruntule.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 class qr_giris_goruntule extends StatefulWidget{
 
+final String tarih;
 final String Kno;
+final String kadsoyad;
 
-qr_giris_goruntule(this.Kno);
+qr_giris_goruntule(this.Kno,this.tarih,this.kadsoyad);
 
   @override
   State<StatefulWidget> createState() => _qr_giris_goruntule();
@@ -12,16 +16,33 @@ qr_giris_goruntule(this.Kno);
 }
 
 class _qr_giris_goruntule extends State<qr_giris_goruntule>{
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Qr Giriş Görüntüle"),
+        title: Text("Giriş Çıkış Saatleri"),
       ),
       body: Center(
         child: Column(
           children: <Widget>[
-            Text("asd:" + widget.Kno),
+            Container(
+                padding: EdgeInsets.fromLTRB(5,0,5,0),
+                height: 150,
+                width: double.maxFinite,
+                child: Card(
+                  color: Colors.amber,
+                  elevation: 5,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text("Kullanıcı Ad Soyad: " + widget.kadsoyad),
+                      Text("Kullanıcı No: " + widget.Kno),
+                      Text("Tarih: " + widget.tarih),
+                    ],
+                  ),
+                ),
+              ),
           ],
         ),
       ),
--- securityqr/lib/admin/qr_oku.dart
+++ securityqr/lib/admin/qr_oku.dart
@@ -2,8 +2,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'dart:io';
-
+import 'package:intl/intl.dart';
 import 'package:securityqr/admin/qr_giris_goruntule.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class qr_oku extends StatefulWidget{
   @override
@@ -12,9 +15,44 @@ class qr_oku extends StatefulWidget{
 }
 
 class _qr_oku extends State<qr_oku>{
+  
   final GlobalKey qrKey = GlobalKey(debugLabel: 'OK');
   Barcode? result;
   QRViewController? controller;
+
+  String tarih = DateFormat('yyyy-MM-dd kk:mm:ss').format(DateTime.now());
+  
+
+
+  CollectionReference ref = FirebaseFirestore.instance.collection('GirisCikis');
+  CollectionReference refqrsec = FirebaseFirestore.instance.collection('qr-security');
+  final Stream<QuerySnapshot> _userStream = FirebaseFirestore.instance.collection('GirisCikis').snapshots();
+
+  late String adsoyad ="";
+
+  void bilgiGetir()async{
+    QuerySnapshot querySnapshot = await refqrsec.get();
+    if(result!=null){
+      for(int i = 0;i<querySnapshot.size;i++){
+      if("${result!.code}" == querySnapshot.docChanges[i].doc['kullanıcıno'].toString()){
+        adsoyad = querySnapshot.docChanges[i].doc['adsoyad'];
+        debugPrint("--------------------------");
+        break;
+      }
+    }
+    kullaniciGirisCikis(adsoyad);
+    }
+    else{
+      Text("asd");
+    }
+  }
+  void kullaniciGirisCikis(String adsoyad){
+    ref.add({
+      'tarih': tarih,
+      'kadsoyad':adsoyad,
+      'kno':"${result!.code}",
+    });
+  }
 
   @override
   void ressemble(){
@@ -48,7 +86,10 @@ class _qr_oku extends State<qr_oku>{
               height: 40,
               child: Text("Sayfaya git"),
               onPressed: (){
+                
+               
                 goster();
+                
               }
             ),
           ),
@@ -67,9 +108,12 @@ class _qr_oku extends State<qr_oku>{
   }
   void goster(){
     if(result!=null){
-      Text("${result!.code}");
+      bilgiGetir();
+      adsoyad = adsoyad;
+      //Text("${result!.code}");
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) {
-              return qr_giris_goruntule("${result!.code}");
+              //return qr_giris_goruntule("${result!.code}");
+              return qr_giris_goruntule("${result!.code}",tarih,adsoyad);
             }));
     }
     else{
